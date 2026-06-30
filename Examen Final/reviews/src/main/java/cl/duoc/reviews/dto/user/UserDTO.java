8fc00da0116ffcc27d7878b0507f51cda425a354
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-30 12:01:02 -0400

    dto de userdto

---
+++ Examen Final/reviews/src/main/java/cl/duoc/reviews/dto/user/UserDTO.java
@@ -0,0 +1,15 @@
+package cl.duoc.reviews.dto.user;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String username;
+}
