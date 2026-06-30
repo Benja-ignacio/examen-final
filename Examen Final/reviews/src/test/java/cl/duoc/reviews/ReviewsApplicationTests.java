bf1ab15d4dc2befed21e8a3a9153c095da1bfa05
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-30 12:02:18 -0400

    archivo test de review

---
+++ Examen Final/reviews/src/test/java/cl/duoc/reviews/ReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
