146feea859de638e457f044f66717a8cad2ea8fe
seiiwl <69690921+Benja-ignacio@users.noreply.github.com>
2026-06-30 12:01:25 -0400

    Modelo de review

---
+++ Examen Final/reviews/src/main/java/cl/duoc/reviews/model/Review.java
@@ -0,0 +1,54 @@
+package cl.duoc.reviews.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "review")
+public class Review {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "username", nullable = false)
+    private String username;       // referencia a Login Service
+
+    @Column(name = "destination_id", nullable = false)
+    private UUID destinationId;     // referencia a Destination Service (solo el ID)
+
+    @Column(name = "rating", nullable = false)
+    @Max(5)
+    @Min(0)
+    private Integer rating;
+
+    @Column(name = "comment", nullable = false)
+    private String comment;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
